8b4f3abcf2712ddc781a5749013ddf750ce405db
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-02 15:51:01 +0100

    Added bootstrap-icons

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 console.log("Hello, Webpack! XXX");
 import './style.css';
 import component from './component';
 import './image-component';
-import 'bootstrap';
+
 
 document.body.appendChild(component());
--- src/component.js
+++ src/component.js
@@ -2,7 +2,15 @@ export default (text = "Hello, Webpack!") => {
     const element = document.createElement("h1");
     element.className = 'text-monospace'
     element.innerHTML = text;
-  
-    return element;
+
+
+    var button = document.createElement("button")
+    button.className = "btn btn-danger btn-lg";
+
+    var icon = document.createElement("i")
+    icon.className = "bi-alarm"
+    button.appendChild(icon);
+
+    return button;
   };
   
