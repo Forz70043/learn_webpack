3f3971ed7750df67106392e781391f6343a30f32
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-02 17:49:36 +0100

    Update app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import 'bootstrap';
+
+import '@popperjs/core/dist/cjs/popper.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './css/style.css';
+import 'bootstrap';
+
+const axios = require('axios');
 
 console.log("Hello, Webpack! XXX");
-import './style.css';
+import header from './header-component';
 import component from './component';
-import './image-component';
+//import './image-component';
+import widget from './widget-component';
 
+//component.appendChild(widget())
+document.body.appendChild(header());
 
 document.body.appendChild(component());
