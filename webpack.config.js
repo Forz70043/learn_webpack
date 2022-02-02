88f0e4d26d87aa7e435cd5016c2c16c1f2c7cb8f
Alfonso <alfonso.pisicchio@libero.it>
2022-02-02 15:03:56 +0100

    Commenti webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
+//WEBPACK PLUGIN added from npm
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
 const path = require('path');
 
 module.exports = {
+  //Define Entry point
     entry: {
         main: path.resolve(__dirname, './src/app.js'),
     },
+    //Define where output bundle
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'deploy')
     },
+    //Just for development, hot reload
     devServer: {
       static: { directory: path.join(__dirname, 'deploy')},
       //contentBase: './deploy',
       open: true,
-      port: 9000
+      port: process.env.PORT || 9000
     },
     module: {
         rules: [
@@ -21,25 +27,30 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
+              //Used for transpling from ES6 to ES5, for old browser
               loader: 'babel-loader',
               options: {
                 presets: ['@babel/preset-env']
               }
             }
           },
-          { 
+          { //Loader for .css file
             test: /\.css$/, 
             use: ["style-loader", "css-loader"] 
           },
-          { 
+          { //Loader for image 
             test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
             type: 'asset/resource',
           },
+          
         ]
     },
     plugins: [
+      //Put title into HTML page
         new HtmlWebpackPlugin({
-        title: "Webpack Output",
+        title: "APP TITLE",
         }),
+        //.env VARS
+        new Dotenv()
     ],
 };
