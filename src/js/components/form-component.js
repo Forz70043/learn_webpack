dc212a88850eea2dc5e006ad9ff6bfcd29c48ba4
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-04 19:46:47 +0100

    Refactor form

--- src/js/components/form-component.js
+++ src/js/components/form-component.js
@@ -1,29 +1,44 @@
+console.log(" in form-component")
 function doRequest(){ console.log(' clicked '+new Date().getTime()); }
-
+console.log(" in form-component dopodichiarazione funct")
 
 
 
 
 
 let form = document.createElement("form");
+form.setAttribute('id', 'cityForm');
+form.className = 'needs-validation';
+form.setAttribute('novalidate', 'true');
+//form = document.createElement("div");
+//form.className = "row"
+
 let div = document.createElement("div");
-div.className = "mb-3";
+div.className = "mb-3 mt-5";
 let label = document.createElement("label");
 label.className = "form-label"
 label.setAttribute('for', 'cityInput');
+label.innerText = "City name must be in english";
 
 let cityInput = document.createElement("input");
-cityInput.className = "form-control form-control-lg";
+cityInput.className = "form-control form-control-lg ";
 cityInput.setAttribute('name', 'cityInput');
 cityInput.setAttribute('id', 'cityInput');
 cityInput.setAttribute('required', 'true');
 cityInput.setAttribute('type', 'text');
+cityInput.setAttribute('minlength', '4');
 cityInput.setAttribute('placeholder', 'City name ...');
+cityInput.setAttribute('aria-describedby', 'invalid-feedback');
+
+let invalidFeedback = document.createElement("div");
+invalidFeedback.className = "invalid-feedback";
+invalidFeedback.setAttribute('id', 'invalid-feedback');
+invalidFeedback.innerHTML = "Please provide a valid city.";
 
 let button = document.createElement("button");
 button.className = "btn btn-primary btn-lg";
-button.setAttribute('onclick', 'doRequest()');
-
+//button.setAttribute('onclick', '()=>{console.log("click")}');
+button.setAttribute('id', 'buttonSubmit');
 
 let iconButton = document.createElement("i");
 iconButton.className = "bi bi-send";
@@ -32,6 +47,7 @@ button.appendChild(iconButton);
 
 div.appendChild(label);
 div.appendChild(cityInput);
+div.appendChild(invalidFeedback);
 form.appendChild(div);
 form.appendChild(button);
 
@@ -52,3 +68,4 @@ formRow.appendChild(col_1)
 formRow.appendChild(col_2)  
 
 document.getElementById('container').appendChild(formRow);
+console.log(" in form-component alla fine di tutto ")
