90cc862b58d7d9e9a057f8138f7b12d0ab329e98
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-04 19:45:40 +0100

    Added utils

---
+++ src/utils.js
@@ -0,0 +1,94 @@
+
+import Chart from 'chart.js/auto';
+console.log(" in utils")
+
+function xxx(){
+    console.log("xxx");
+}
+
+function checkValidity(){
+    if(!document.getElementById('cityForm').checkValidity()){
+        document.getElementById('cityInput').classList.add('is-invalid');
+        return false;
+    }
+    else{
+        document.getElementById('cityForm').classList.add('was-validated');
+        document.getElementById('cityInput').classList.remove('is-invalid');
+        //add('was-validated');
+
+        //api(document.getElementById('cityInput').value);
+        return true;
+    }
+}
+
+function chartInit(params){
+    console.log('chartInit');
+    console.log("XXXX init: ",params);
+
+    var x = [];
+    var y = [];
+    document.getElementById('chart_1').classList.remove('d-none');
+    params.data.forEach(row => {
+        x.push(row.name);
+        y.push(row.score_out_of_10);    
+    });
+    
+    console.log(
+        x,
+        y
+    );
+
+    var barColors = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)',
+        //'rgba(5, 247, 5, 0.2)'
+    ];
+
+    var chart = new Chart("chart_1", {
+        type: "bar",
+        data: {
+            labels: x,
+            datasets: [{
+              label: ' ',
+              data: y,
+              backgroundColor: barColors,
+              borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+              ],
+              borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "World Wide Wine Production"
+            },
+            indexAxis: 'y'
+        }
+    });
+
+
+    return chart;
+}
+
+
+export default (params, options)=>{ 
+    console.log(' ci sono '+new Date().getTime()); 
+    if(params == 'chartInit' && options){
+        chartInit(options);
+    }
+    else if(params =='checkValidity'){
+        return checkValidity();
+    }
+}
