0b81d50c2a491517d59c80bb593fee67305f052b
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-08 16:30:59 +0100

    Delete image comp.

--- src/image-component.js
+++
@@ -1,6 +0,0 @@
-import image from "./img/image.png";
-
-const img = document.createElement("img");
-img.src = image;
-document.body.appendChild(img);
-//document.body ="<div class=\"container\"><div class=\"alert alert-primary\" role=\"alert\">This is a primary alert—check it out!</div></div>";
