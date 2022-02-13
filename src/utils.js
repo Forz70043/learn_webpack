5c5ab789d087c0e796b262767217c0dab6ffecc3
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-13 10:48:56 +0100

    Refactor utils

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,91 @@
 
-import Chart from 'chart.js/auto';
-console.log(" in utils")
-import notice from './js/components/notice-component';
+import * as dash from './js/components/charts.component';
 
-function xxx(){
-    console.log("xxx");
+let chart; 
+
+/**
+ * Replace space with -
+ * return lowercase string
+ */
+export function formatInput(input){
+    return input.toLowerCase().replace(' ','-');
 }
 
-function _catch(params){
-    console.log("catch");
-    notice(params);
+function showNotice(){
+    document.getElementById('alertNotice').classList.add('show');
+    document.getElementById('alertNotice').classList.remove('d-none');
 }
 
+function hideNotice(){
+    document.getElementById('alertNotice').classList.add('d-none');
+    document.getElementById('alertNotice').classList.remove('show');
+}
+
+export function showHideNotice(){
+    (document.getElementById('alertNotice').classList.contains('d-none') || !document.getElementById('alertNotice').classList.contains('show')) 
+    ?  showNotice() : hideNotice();
+    //document.getElementById('alertNotice').classList.add('show');
+}
+
+export function _catch(params){
+    console.log("catch", params);
+    //notice(params);
+    //return showHideNotice();
+
+}
+
+
+/**
+ * 
+ * @param {*} id element
+ * @returns true or flase if elem contains class 'is-invalid'
+ */
+function isInvalid(id){
+    if(!id) return false;
+    if(document.getElementById(id).contains('is-invalid')) return true;
+    else return false;
+}
+
+export function setInvalid(){
+    document.getElementById('cityInput').classList.add('is-invalid'); 
+    document.getElementById('cityInput').classList.remove('is-valid');    
+    //document.getElementById('cityForm').classList.remove('was-validated');
+}
+
+function setValid(){
+    //document.getElementById('cityForm').classList.add('was-validated');
+    document.getElementById('cityInput').classList.remove('is-invalid');
+    document.getElementById('cityInput').classList.add('is-valid');
+}
+
+function isEmpty(val){
+    if(val === '' || val == '' || val === undefined || val === null) return true;
+    else return false;
+}
+
+export function get(id){
+    if(!id) return false;
+    return document.getElementById(id);
+}
+
+export function getValue(id){
+    let val = document.getElementById(id).value;
+
+    if(!isEmpty(val)) return val;
+    else return false;
+    
+}
 
 function checkValidity(){
-    if(!document.getElementById('cityForm').checkValidity()){
-        document.getElementById('cityInput').classList.add('is-invalid');
-
-        document.getElementById('chart_1').classList.add('d-none');
+    console.log("XXXXXXXXXXXXXXXXXXXXXX")
+    if(!get('cityForm').checkValidity()){
+        setInvalid();
         return false;
     }
     else{
-        document.getElementById('cityForm').classList.add('was-validated');
-        document.getElementById('cityInput').classList.remove('is-invalid');
+        //if(notice)
+        setValid();
         //add('was-validated');
-
-        //api(document.getElementById('cityInput').value);
         return true;
     }
 }
@@ -34,9 +94,12 @@ function chartInit(params){
     console.log('chartInit');
     console.log("XXXX init: ",params);
 
+    chart = dash.chartInit('chart_1')
+
     var x = [];
     var y = [];
     document.getElementById('chart_1').classList.remove('d-none');
+
     params.data.categories.forEach(row => {
         x.push(row.name);
         y.push(row.score_out_of_10);    
@@ -47,7 +110,7 @@ function chartInit(params){
         y
     );
 
-    var barColors = [
+    let barColors = [
         'rgba(255, 99, 132, 0.2)',
         'rgba(255, 159, 64, 0.2)',
         'rgba(255, 205, 86, 0.2)',
@@ -57,8 +120,8 @@ function chartInit(params){
         'rgba(201, 203, 207, 0.2)',
         //'rgba(5, 247, 5, 0.2)'
     ];
-
-    var chart = new Chart("chart_1", {
+    
+    chart = new Chart("chart_1", {
         type: "bar",
         data: {
             labels: x,
@@ -79,23 +142,39 @@ function chartInit(params){
             }]
         },
         options: {
-            title: {
-                display: true,
-                text: "World Wide Wine Production"
-            },
-            indexAxis: 'y'
+            indexAxis: 'y',
+            plugins: {
+                /*legend: {
+                    display: true,
+                    labels: {
+                        color: 'rgb(255, 99, 132)'
+                    }
+                },*/
+                title: {
+                    display: true,
+                    text: "World City Life Quality"
+                },
+            }
         }
     });
-
 
     return chart;
 }
 
 
-export default (params, options)=>{ 
-    console.log(' ci sono '+new Date().getTime()); 
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function call(params, options) {
+
+    console.log('in utils '+new Date().getTime()); 
     if(params == 'chartInit' && options){
-        chartInit(options);
+        return chartInit(options);
     }
     else if(params =='checkValidity'){
         return checkValidity();
@@ -104,3 +183,46 @@ export default (params, options)=>{
         return _catch();
     }
 }
+
+export function checkForm(){
+    console.log("check form ", document.getElementById('cityForm').checkValidity());
+
+    if(document.getElementById('cityForm').checkValidity()){
+        setValid();
+        return true;
+    }
+    else{
+        setInvalid();
+        return false;
+    }
+}
+
+
+let utils = {
+    get: function(id){ return document.getElementById(id)},
+    call: function(params, options) {
+        console.log('in utils '+new Date().getTime()); 
+        if(params == 'chartInit' && options){
+            return chartInit(options);
+        }
+        else if(params =='catch'){
+            return _catch();
+        }
+    },
+    formatInput: function (input){ return input.toLowerCase().replace(' ','-'); },
+    checkForm: function(){ 
+        if(!document.getElementById('cityForm').checkValidity()){
+            //setInvalid();
+            return false;
+        }
+        else{
+            //if(notice)
+            //setValid();
+            //add('was-validated');
+            return true;
+        }
+    }
+};
+
+export default { formatInput, checkForm, get, getValue, _catch, setInvalid, showHideNotice };
+
