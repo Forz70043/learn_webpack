02bd5ae76255eb529968aacc3c59f1a11fc36b59
Forz70043 <alfonso.pisicchio@libero.it>
2022-02-13 10:50:30 +0100

    Webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 //WEBPACK PLUGIN added from npm
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
+//const nodeExternals = require('webpack-node-externals');
 
 const path = require('path');
 
 module.exports = {
-  //Define Entry point
+    //Define Entry point
     entry: {
         main: path.resolve(__dirname, './src/app.js'),
     },
     //Define where output bundle
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'deploy')
+        path: path.resolve(__dirname, 'deploy'),
+        //publicPath: '/',
     },
+    /*
+    target: 'node',
+    node: {
+      // Need this when working with express, otherwise the build fails
+      __dirname: false,   // if you don't put this is, __dirname
+      __filename: false,  // and __filename return blank or /
+    },
+    externals: [nodeExternals()], // Need this to avoid error when working with Express
+    */
     //Just for development, hot reload
     devServer: {
       static: { directory: path.join(__dirname, 'deploy')},
@@ -48,7 +59,10 @@ module.exports = {
     plugins: [
         //Put title into HTML page
         new HtmlWebpackPlugin({
+          //template: './deploy/index.html',
+          //filename: './deploy/index.html',
           title: "Life City Quality",
+          //excludeChunks: [ 'server' ]
         }),
         //.env VARS
         new Dotenv()
